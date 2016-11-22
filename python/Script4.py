21e5b3e8c8744fff83775ec6cceec8c3b72f8ce4
Esteban Osorio <e.osorio.gallardo@gmail.com>
2016-11-22 19:59:30 -0300

    sincronizacion

---
+++ python/Script4.py
@@ -0,0 +1,6 @@
+#/usr/bin/python
+emails = ['me@gmail', 'you@hotmail', 'they@gmail']
+
+for email in emails:
+    if 'gmail' in email:
+        print(email)
--- python/celsius2farenheit.py
+++ python/celsius2farenheit.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
+
 print("Celsius value 5")
 
 
 def cel2far(c):
 
-    f = c * 9 / 5 + 32
-    return f
+    if c < -273.15 :
+        print("Error, value out of range")
+        return
+    else:
+        f = c * 9 / 5 + 32
+        return f
 
-print("Fahrenheit=" + str(cel2far(5)))
+print("Fahrenheit=" + str(cel2far(-274)))
