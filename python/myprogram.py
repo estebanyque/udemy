339286af3f8c8c9c9ed07c89769ae85fca83cfba
Esteban Osorio <e.osorio.gallardo@gmail.com>
2017-07-03 18:39:58 -0400

    ...

--- python/myprogram.py
+++ python/myprogram.py
@@ -1,1 +1,26 @@
-print("Hello")
+#greeting2 = input("Write a greeting: ")
+#print(greeting2)
+
+#a = 2
+#b = 3.5
+#print(a + b)
+#print(type(a))
+age = input("Enter your age: ")
+new_age = int(age) + 50
+print(new_age)
+
+#a = 20 - 10/5 * 3**2
+#b = (20 - 10) / 5 * 3**2
+#c = (20 - 10) / (5 * 3)**2
+#print(a)
+#print(b)
+#print(c)
+
+c = "Here"
+print(c.replace("e", "i"))
+
+# List methods
+print(dir(c))
+
+#Help
+print(help("".replace))
