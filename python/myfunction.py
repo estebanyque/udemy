54d358b274802f3b4168212bf6307a3f26306122
Esteban Osorio <e.osorio.gallardo@gmail.com>
2017-07-04 18:17:04 -0400

    ...

---
+++ python/myfunction.py
@@ -0,0 +1,7 @@
+def minutes_to_hours(minutes):
+    hours = float(minutes) / 60
+    return hours
+
+#minutes = input("How many minutes: ")
+#print(minutes_to_hours(minutes))
+print(minutes_to_hours(70))
